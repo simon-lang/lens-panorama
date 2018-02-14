18f24ef4dc18655ad1d0a27d50b22142c9d2e6ae
Simon Lang <simon.lang@cambia.org>
2018-02-14 13:25:24 +1000

    Use proxy. Fix patent facets endpoint

--- src/constants.ts
+++ src/constants.ts
@@ -1,3 +1,3 @@
 export const BASE_URL: string = 'http://localhost:9083/lens'
-export const BASE_SCHOLARLY_API_URL: string = 'http://localhost:9080/lens/api'
-export const BASE_PLATCLASS_API_URL: string = 'https://patclass.api.lens.org/rest/v1.0'
+export const BASE_SCHOLARLY_API_URL: string = 'http://localhost:9083/lens/api'
+export const BASE_PLATCLASS_API_URL: string = 'http://localhost:9083/patclass'
--- src/services/PatentService.ts
+++ src/services/PatentService.ts
@@ -5,15 +5,13 @@ import { Patent } from '../models/Patent'
 
 export class PatentService {
     search(q: string): Promise<Patent[]> {
-        // const encodedQ: string = window.encodeURIComponent(q)
         return fetch(`${BASE_URL}/search?q=${q}&json=1`).then((d) => d.json()).then((d) => {
             return d.result.hits.map((patent) => new Patent(patent))
         })
     }
     facets(q: string): Promise<Patent[]> {
-        // const encodedQ: string = window.encodeURIComponent(q)
-        return fetch(`${BASE_URL}/facetData?q=${q}`).then((d) => d.json()).then((d) => {
-            return d.result.hits.map((patent) => new Patent(patent))
+        return fetch(`${BASE_URL}/search/facetData?q=${q}`).then((d) => d.json()).then((d) => {
+            return d.facets
         })
     }
 }
