05680dd84d0f30a6cec0ddafeedaac8ddf5e3b8a
Simon Lang <simon.lang@cambia.org>
2018-02-16 10:18:52 +1000

    Auto format component test

--- src/components/client/client.spec.ts
+++ src/components/client/client.spec.ts
@@ -3,17 +3,17 @@ import { ClientComponent } from './client'
 import { ComponentTest } from '../../util/component-test'
 
 describe('Home component', () => {
-  let directiveTest: ComponentTest
+    let directiveTest: ComponentTest
 
-  beforeEach(() => {
-    directiveTest = new ComponentTest('<div><home></home></div>', { 'home': ClientComponent })
-  })
-
-  it('should render correct contents', async () => {
-    directiveTest.createComponent()
-    await directiveTest.execute((vm) => {
-      const mode = process.env.ENV
-        expect(vm.$el.querySelector('h2').textContent).to.equal(`What can I search for?`)
+    beforeEach(() => {
+        directiveTest = new ComponentTest('<div><home></home></div>', { 'home': ClientComponent })
     })
-  })
+
+    it('should render correct contents', async () => {
+        directiveTest.createComponent()
+        await directiveTest.execute((vm) => {
+            const mode = process.env.ENV
+            expect(vm.$el.querySelector('h2').textContent).to.equal(`What can I search for?`)
+        })
+    })
 })
