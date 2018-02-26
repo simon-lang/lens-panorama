94b9874f77536c42da75ec2882d8a31661e742b0
Simon Lang <simon.lang@cambia.org>
2018-02-26 15:21:58 +1000

    Temporarily disable tests. They are being bundled

--- src/services/ArticleService.spec.ts
+++ src/services/ArticleService.spec.ts
@@ -3,19 +3,19 @@ const service: ArticleService = new ArticleService()
 
 const sampleResponse = require('../../data/search-api/aggregations.json')
 
-describe('ArticleService', () => {
-    it('createFacetsFromScholarlyAggs', () => {
-        const { aggregations } = sampleResponse.query_result
-        const facets = service.createFacetsFromScholarlyAggs(aggregations)
+// describe('ArticleService', () => {
+//     it('createFacetsFromScholarlyAggs', () => {
+//         const { aggregations } = sampleResponse.query_result
+//         const facets = service.createFacetsFromScholarlyAggs(aggregations)
 
-        const facet = facets[0]
-        expect(facet.key).toEqual('funding.organisation.keyword')
-        expect(facet.type).toEqual('scholar')
-        expect(facet.values[0].key).toEqual('NHLBI NIH HHS')
-        expect(facet.values[0].value).toEqual(196)
+//         const facet = facets[0]
+//         expect(facet.key).toEqual('funding.organisation.keyword')
+//         expect(facet.type).toEqual('scholar')
+//         expect(facet.values[0].key).toEqual('NHLBI NIH HHS')
+//         expect(facet.values[0].value).toEqual(196)
 
-        const singleValueFacet = facets[1]
-        expect(singleValueFacet.key).toEqual('sum_referenced_by_patent_count')
-        expect(singleValueFacet.value).toEqual(317)
-    })
-})
+//         const singleValueFacet = facets[1]
+//         expect(singleValueFacet.key).toEqual('sum_referenced_by_patent_count')
+//         expect(singleValueFacet.value).toEqual(317)
+//     })
+// })
--- src/services/PatentService.spec.ts
+++ src/services/PatentService.spec.ts
@@ -3,15 +3,15 @@ const service: PatentService = new PatentService()
 
 const sampleResponse = require('../../data/facets/Rice-University.json')
 
-describe('PatentService', () => {
-    it('createFacetsFromRawData', () => {
-        const facets = service.createFacetsFromRawData(sampleResponse.facets)
+// describe('PatentService', () => {
+//     it('createFacetsFromRawData', () => {
+//         const facets = service.createFacetsFromRawData(sampleResponse.facets)
 
-        const facet = facets[0]
-        expect(facet.key).toEqual('owner')
-        expect(facet.type).toEqual('patent')
-        expect(facet.values[0].key).toEqual('INTERNATIONAL BUSINESS MACHINES CORPORATION')
-        expect(facet.values[0].label).toEqual('International Business Machines Corporation')
-        expect(facet.values[0].value).toEqual(206)
-    })
-})
+//         const facet = facets[0]
+//         expect(facet.key).toEqual('owner')
+//         expect(facet.type).toEqual('patent')
+//         expect(facet.values[0].key).toEqual('INTERNATIONAL BUSINESS MACHINES CORPORATION')
+//         expect(facet.values[0].label).toEqual('International Business Machines Corporation')
+//         expect(facet.values[0].value).toEqual(206)
+//     })
+// })
