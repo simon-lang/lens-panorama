ada5a8073da1fc2f8a818b83c1e58495f8b35e91
Simon Lang <simon.lang@cambia.org>
2018-02-27 15:42:29 +1000

    Translations

--- src/messages.ts
+++ src/messages.ts
@@ -1,16 +1,3 @@
-export default {
-    en: {
-        message: {
-            'home': 'Home',
-            'patentField.publicationDate': 'Publication Date',
-            'patentField.publicationNumber': 'Publication Number',
-        }
-    },
-    es: {
-        message: {
-            'home': 'Casa',
-            'patentField.publicationDate': 'Fecha de Publicación',
-            'patentField.publicationNumber': 'Número de Publicación',
-        }
-    }
-}
+const messages = require('../data/messages-condensed.json')
+
+export default messages
